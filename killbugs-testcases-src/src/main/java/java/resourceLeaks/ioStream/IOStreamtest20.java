0ea30ccee07def449dd70cfa56271f4ed693382f
Luohui <93994822@qq.com>
2018-01-10 22:28:26 +0800

    added some bugs about resource leak.

---
+++ killbugs-testcases-src/src/main/java/java/resourceLeaks/ioStream/IOStreamtest20.java
@@ -0,0 +1,47 @@
+package java.resourceLeaks.ioStream;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class IOStreamtest20 {
+	public void foo(){
+//		int s = 0;
+		Xclass1 xclass = new Xclass1();
+		Yclass yclass1 = new Yclass();
+		Yclass yclass2 = new Yclass();
+		bar(xclass);
+		set(xclass, yclass1);
+		set(xclass, yclass2);
+		close(yclass1);
+		
+	}
+		
+	void bar(Xclass1 xclass){
+		try{
+			FileOutputStream fOutputStream  = new FileOutputStream(new File(""));
+			xclass.fOutputStream = fOutputStream;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	void set(Xclass1 x,Yclass y){
+		y.xclass = x;
+	}
+	void close(Yclass yclass1){
+		try {
+			yclass1.xclass.fOutputStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class Yclass{
+	static Xclass1 xclass;
+}
+class Xclass1{
+	FileOutputStream fOutputStream;
+}
---
+++ killbugs-testcases-src/src/main/java/java/resourceLeaks/ioStream/IOStreamtest5.java
@@ -0,0 +1,32 @@
+package java.resourceLeaks.ioStream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class IOStreamtest5 {
+	public void foo(FileInputStream fis){
+		FileInputStream temp_fis = null;
+		try{
+		fis= new FileInputStream(new File("1.txt"));
+		temp_fis = fis;
+		fis=new FileInputStream(new File("2.txt"));
+		}
+		catch (IOException e){
+		e.printStackTrace();	
+		}
+		finally{
+			try {
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (temp_fis != null)
+				try {
+					temp_fis.close();
+				} catch (IOException te1) {
+				}
+		}
+	}
+}
